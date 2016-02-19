8e1c12e9cbf18269f74c44fed152d09be9ac2ad6
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-02-19 10:22:21 -0500

    Add hsv visual image

--- src/pixelColors.py
+++ src/pixelColors.py
@@ -25,9 +25,9 @@ def print_mouse(event,x,y,flags,param):
         x0 = min(param["down"][1],x)
         x1 = max(param["down"][1],x)+1
 
-        mean = np.mean(param["frame"][y0:y1,x0:x1,:],axis=(0,1))
-        lower = np.percentile(param["frame"][y0:y1,x0:x1,:],2.5,axis=(0,1))
-        upper = np.percentile(param["frame"][y0:y1,x0:x1,:],97.5,axis=(0,1))
+        mean = np.mean(param["hsv"][y0:y1,x0:x1,:],axis=(0,1))
+        lower = np.percentile(param["hsv"][y0:y1,x0:x1,:],2.5,axis=(0,1))
+        upper = np.percentile(param["hsv"][y0:y1,x0:x1,:],97.5,axis=(0,1))
 
         print "HSV mean:  [% 4d,% 4d,% 4d]" % tuple(mean)
         print "HSV lower: [% 4d,% 4d,% 4d]" % tuple(lower)
@@ -38,20 +38,25 @@ def print_mouse(event,x,y,flags,param):
 camera = cv2.VideoCapture(2)
 grabbed,frame = camera.read()
 frame = imutils.resize(frame, width=600)
+hsvFrame = cv2.cvtColor(frame.copy(),cv2.COLOR_BGR2HSV)
 
-param = {"down":None,"frame":cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)}
+param = {"down":None,"hsv":hsvFrame.copy()}
 
-cv2.namedWindow("frame")
-cv2.setMouseCallback("frame",print_mouse,param=param)
+cv2.namedWindow("bgr")
+cv2.namedWindow("hsv")
+cv2.setMouseCallback("bgr",print_mouse,param=param)
+cv2.setMouseCallback("hsv",print_mouse,param=param)
 
 while True:
-    cv2.imshow("frame",frame)
+    cv2.imshow("bgr",frame)
+    cv2.imshow("hsv",hsvFrame)
     k = cv2.waitKey(20) & 0xFF
     if k == ord("q"):
         break
     elif k == ord("r"):
         grabbed,frame = camera.read()
         frame = imutils.resize(frame, width=600)
-        param["frame"] = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        hsvFrame = cv2.cvtColor(frame.copy(),cv2.COLOR_BGR2HSV)
+        param["hsv"] = hsvFrame.copy()
 
 cv2.destroyAllWindows()
