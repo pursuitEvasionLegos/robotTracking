0f3aa11a7f287a098d8807a39c32b987da05dbea
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-03-08 16:54:18 -0500

    Use homography to transform points

--- scripts/findObjectPoint.py
+++ scripts/findObjectPoint.py
@@ -14,28 +14,52 @@ cam = cv2.VideoCapture(d)
 cam.set(3,1920)
 cam.set(4,1080)
 
-cv2.namedWindow("frame")
-cv2.setMouseCallback("frame",mouse_callback,
 
-def print_mouse(event,x,y,flags,param):
+def mouse_callback(event,x,y,flags,param):
     if event == 1:
-        param["down"] = (y,x)
-    elif event == 4:
+        click = param["click"]
+        if click < 0:
+            if all(param[i]["point"] for i in range(4)):
+                src = np.array([[(x,y)]],
+                               dtype="float32")
+                dst = cv2.perspectiveTransform(src=src,
+                                              m=param["mat"])
+                pts = tuple([x,y] + list(dst[0][0]))
+                print "Transform: (%d,%d) --> (%f,%f)" % pts
 
-        print "down: %s" % str(param["down"])
-        print "up: %s" % str((x,y))
-        y0 = min(param["down"][0],y)
-        y1 = max(param["down"][0],y)+1
-        x0 = min(param["down"][1],x)
-        x1 = max(param["down"][1],x)+1
+            else:
+                print "Need to calibrate points."
 
-        mean = np.mean(param["hsv"][y0:y1,x0:x1,:],axis=(0,1))
-        lower = np.percentile(param["hsv"][y0:y1,x0:x1,:],2.5,axis=(0,1))
-        upper = np.percentile(param["hsv"][y0:y1,x0:x1,:],97.5,axis=(0,1))
+        else:
+            param[click]["point"] = (x,y)
+            param["click"] = click + 1
+            if param["click"] == 4:
+                param["click"] = -1
 
-        print "HSV mean:  [% 4d,% 4d,% 4d]" % tuple(mean)
-        print "HSV lower: [% 4d,% 4d,% 4d]" % tuple(lower)
-        print "HSV upper: [% 4d,% 4d,% 4d]" % tuple(upper)
+                src = np.array([param[i]["point"] for i in range(4)],
+                               dtype="float32")
+                param["mat"] = cv2.getPerspectiveTransform(src=src,
+                                                           dst=param["dst"])
+
+
+clickToCoords = [(0,0),(0,1),(1,0),(1,1)]
+
+data = {"click": -1,
+        "point": None,
+        "dst": np.array(clickToCoords,dtype="float32"),
+        -1: None}
+
+for i in range(4):
+    data[i] = {"asked": False,
+               "point":None}
+
+cv2.namedWindow("frame")
+cv2.setMouseCallback("frame",mouse_callback,param=data)
+
+
+
+
+
 
 
 
@@ -43,25 +67,26 @@ while True:
     if cam is not None:
         grabbed,frame = cam.read()
         if grabbed:
-            frame = imutils.resize(frame,width=800)
-            cv2.imshow("cam %d" % d,frame)
+            frame = imutils.resize(frame,width=1200)
+            cv2.imshow("frame",frame)
 
     k = cv2.waitKey(20) & 0xFF
     if k == ord("q"):
         break
-    elif k in dVals:
-        for i in range(10):
-            if k == ord(str(i)) and i is not d:
-                newCam = cv2.VideoCapture(i)
-                if newCam.isOpened():
-                    cv2.destroyWindow("cam %d" % d)
-                    cam.release()
 
-                    cam = newCam
-                    cam.set(3,1920)
-                    cam.set(4,1080)
-                    d = i
-                break
+    elif k == ord("c"):
+        data["click"] = 0
+        for i in range(4):
+            data[i] = {"asked":False,
+                       "point":None}
+
+    if data["click"] >= 0:
+        if not data[data["click"]]["asked"]:
+            data[data["click"]]["asked"] = True
+
+            print "Select (%d,%d)" % clickToCoords[data["click"]]
+
+
 
 
 cam.release()
